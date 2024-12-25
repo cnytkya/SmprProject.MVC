c5507ca6f2beb3e47f9588a6ad75d8526c39de6d
cnytkya <kayacuneyt2542@gmail.com>
2024-12-25 12:07:22 +0300

    Add project files.

---
+++ SmpProject.MVC/Models/AppDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SmpProject.MVC.Models
+{
+    public class AppDbContext : DbContext //AppDbContext, Entity Framework Core kullanarak bir veritabanı bağlamı (DbContext) tanımlayan bir C# sınıfıdır. DbContext, Entity Framework Core'un veritabanı işlemlerini yönetmek için kullandığı temel sınıftır. Bu sınıf, veritabanı bağlantılarını ve veritabanı işlemlerini yönetir.
+    {
+    }
+}
